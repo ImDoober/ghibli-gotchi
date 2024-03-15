4f0bed1f88334223ef14ecdfb15840609b548e28
ImDoober <152272376+ImDoober@users.noreply.github.com>
2024-03-15 13:35:05 +0100

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,22 @@
+const feed = document.querySelector ("#btn.Feed")
+const work = document.querySelector ("#btn.Work")
+const music = document.querySelector ("#btn.Music")
+const songs = []
+
+feed.addEventListener ('click',feeding)
+
+function feeding(){
+
+}
+
+work.addEventListener ('click',working)
+
+function working(){
+
+}
+
+music.addEventListener ('click',listening)
+
+function listening(){
+
+}
