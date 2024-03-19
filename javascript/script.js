1c98d06ff17eca98b7b4c05e9da08774892152b6
ImDoober <152272376+ImDoober@users.noreply.github.com>
2024-03-19 11:47:44 +0100

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,64 @@
-const feed = document.querySelector ("#btn.Feed")
-const work = document.querySelector ("#btn.Work")
-const music = document.querySelector ("#btn.Music")
-const songs = []
+const feed = document.getElementById ("btn-feed");
+const work = document.getElementById ("btn-work");
+const music = document.getElementById ("btn-music");
+const songs = ["Elevator-music.mp3", "Jazz-music.mp3", "Rock-music.mp3",];
 
-feed.addEventListener ('click',feeding)
+let sprite = document.querySelector (".image");
+let spriteNeutral = true;
+let spriteCandies = 1;
+let timesFed = 0;
+let timesWorked = 0;
+let candyAmount = document.querySelector("p")
+let randomCandies = Math.random () * 3;
+randomCandies = Math.round (randomCandies);
 
 function feeding(){
-
+    if (spriteNeutral == true || spriteCandies >= 1){
+        sprite.src = "images/sprite-food.png";
+        spriteNeutral = false;
+        timesFed++;
+        spriteCandies--;
+        candyAmount.textContent = spriteCandies;
+    } else if (spriteCandies == 0){
+        console.log("You don't have enough candies.");
+        sprite.src = "images/sprite-neutral.png";
+    } else if (timesFed == 12) {
+        sprite.src = "images/sprite-";
+        spriteNeutral = false;
+    } else {
+        sprite.src = "images/sprite-neutral.png";
+        spriteNeutral = true
+    }
 }
-
-work.addEventListener ('click',working)
 
 function working(){
-
+    if ( spriteNeutral == true || timesWorked <= 2){
+        sprite.src = "images/sprite-work.png";
+        spriteNeutral = false;
+        spriteCandies = spriteCandies + randomCandies;
+        candyAmount.textContent = spriteCandies;
+        timesWorked++;
+    } else if (timesWorked >= 2) {
+        console.log("Your sprite is tired.");
+        sprite.src = "images/sprite-neutral.png";
+    } else {
+        sprite.src = "images/sprite-neutral.png";
+        spriteNeutral = true
+    }
 }
-
-music.addEventListener ('click',listening)
 
 function listening(){
-
+    console.log("Listened");
+    if ( spriteNeutral == true){
+        sprite.src = "images/sprite-dance.png";
+        spriteNeutral = false;
+        timesWorked--;
+    } else {
+        sprite.src = "images/sprite-neutral.png";
+        spriteNeutral = true;
+    }
 }
+
+music.addEventListener('click', listening);
+work.addEventListener('click', working);
+feed.addEventListener('click', feeding);
